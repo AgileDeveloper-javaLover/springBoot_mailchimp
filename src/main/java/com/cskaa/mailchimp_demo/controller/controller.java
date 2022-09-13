6cac8295b1e62565b4cd921c84194c210e735725
nisha <nishantprajapati999@gmail.com>
2022-09-13 22:10:57 +0530

    Updated controller.

--- src/main/java/com/cskaa/mailchimp_demo/controller/controller.java
+++ src/main/java/com/cskaa/mailchimp_demo/controller/controller.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,10 @@ public class controller {
 //        System.out.println(mailEvents);
 //    }
 
+    @GetMapping("health")
+    public void getHealth(){
+        System.out.println("Application is running");
+    }
 
     @PostMapping(value = "/mail-processing")
     public void mailProcessing(HttpServletRequest request){
